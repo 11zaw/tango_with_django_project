bff38e872b9db6e51afe8ecce2c70266c83f7858
Zaw Linn Aye <2951053a@student.gla.ac.uk>
2025-02-06 14:44:31 +0000

    End of chapter 5

--- populate_rango.py
+++ populate_rango.py
@@ -33,7 +33,7 @@ def populate():
             "Other Frameworks": {"pages": other_pages, "likes": 16, "views": 32} }
 
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
+        c = add_cat(cat, cat_data["views"], cat_data["likes"])
         for p in cat_data["pages"]:
             add_page(c, p["title"], p["url"])
 
@@ -48,8 +48,10 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
 
-def add_cat(name):
-    c = Category.objects.get_or_create(name=name)[0]
+def add_cat(name, views = 0, likes = 0):
+    c = Category.objects.get_or_create(name=name, defaults={'views': views, 'likes': likes})[0]
+    c.views = views
+    c.likes = likes
     c.save()
     return c
 
